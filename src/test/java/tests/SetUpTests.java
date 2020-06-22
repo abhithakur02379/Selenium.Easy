860b144c40fca7935baaed22ce0a15e96af002c0
Abhishek <abhithakur02379@gmail.com>
2020-06-22 10:45:04 +0530

    Code update on 22_June_01

--- src/test/java/tests/SetUpTests.java
+++ src/test/java/tests/SetUpTests.java
@@ -1,10 +1,11 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.Reporter;
-import testutilities.InputRead;
+import org.testng.annotations.BeforeTest;
+
+import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -12,29 +13,20 @@ import static com.codeborne.selenide.Selenide.open;
 public class SetUpTests {
 
 
+    public static final Properties config = new Properties();
 
+    @BeforeTest(description = "Test to verify application launched successfully")
+    public void launch_ParaBank () throws Exception {
 
-//    @BeforeMethod(description = "Test to verify application launched successfully")
-    public void launch_ParaBank () throws InterruptedException
-    {
-
+        config.load(SetUpTests.class.getClassLoader().getResourceAsStream("config.properties"));
         System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\Drivers\\msedgedriver.exe");
         System.setProperty("selenide.browser", "Edge");
         Thread.sleep(1000);
         Configuration.startMaximized = true;
         Configuration.reportsFolder = System.getProperty("user.dir") + "\\test-output";
-        open(InputRead.getProperty("baseURI"));
+        open(config.getProperty("baseURI"));
         Thread.sleep(1000);
-        if ($(By.xpath("//a[contains(text(),'ParaBank')]")).isDisplayed())
-        {
-            Reporter.log("Home Page Loaded");
-            Assert.assertEquals("Home Page Loaded","Home Page Loaded");
-        }
-        else
-        {
-            Reporter.log("Home Page NOT Loaded");
-            Assert.assertEquals("Home Page NOT Loaded","Home Page Loaded");
-        }
+        $(By.xpath("//a[contains(text(),'ParaBank')]")).shouldBe(Condition.visible);
     }
 
 
